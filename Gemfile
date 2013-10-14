034387e70973d04eab3a6c916f0011b94ce4eec7
Isaac Priestley <progressions@gmail.com>
2013-10-14 16:10:50 -0500

    remove growl

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
 
-gem "ruby-growl", ">= 0"
-gem "progressions-g", ">= 0"
 gem "activesupport", :require => "active_support"
 gem "natural_time", ">= 0"
 
--- lib/timer.rb
+++ lib/timer.rb
@@ -1,21 +1,17 @@
 require 'rubygems'
-require 'g'
 require 'natural_time'
 
 class Timer
-  attr_accessor :title, :growl
-  
-  DEFAULT_OPTIONS = { :growl => true }
-  
+  attr_accessor :title
+
   def initialize(options={})
-    options = DEFAULT_OPTIONS.merge(options)
+    options ||= options
     @title = options[:title]
-    @growl = options[:growl]
   end
-  
+
   def time(message="", options={})
     current_title = options[:title] || title
-    
+
     start_time = Time.now
     begin
       yield
@@ -25,11 +21,7 @@ class Timer
     put_elapsed_time(start_time, Time.now, message, current_title)
     raise e if e
   end
-  
-  def growl?
-    @growl
-  end
-  
+
   private
 
   def put_elapsed_time(start_time, end_time, message="", current_title=nil)
@@ -42,25 +34,9 @@ class Timer
     e = message + e
     e += "Elapsed time: #{elapsed_time}"
     $stdout.puts "\n" + e
-    growl(e, current_title)
   end
-  
+
   def format_time(duration)
     NaturalTime.new(duration).to_sentence
-  end
-  
-  def growl(message, current_title)
-    if growl?
-      if title
-        g(message, :title => current_title)
-      else
-        g(message)
-      end
-    end
-  rescue StandardError => e
-    # growl isn't turned on, fail silently
-    unless e.message =~ /Connection refused/
-      raise e
-    end    
   end
 end
